5bc3f310d4b6a2292f91007a2ccb56daf9c0f95d
Robbert van der Helm <mail@robbertvanderhelm.nl>
2019-11-02 17:04:16 +0100

    Read the image and create pinging streams

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 extern crate clap;
+extern crate image;
 extern crate tokio;
 extern crate tokio_ping;
 
-use clap::{App, Arg};
+use clap::{value_t, value_t_or_exit, App, Arg};
+use image::{FilterType, GenericImageView, Rgba};
 use std::net::{IpAddr, Ipv6Addr};
+use std::process::exit;
 use std::time::Duration;
 use tokio::prelude::*;
 use tokio_ping::Pinger;
 
-/// The rate of the pings in miliseconds.
-const PING_RATE: u64 = 100;
+/// The default rate of the pings in miliseconds.
+const DEFAULT_PING_RATE: &str = "50";
 
 fn main() {
     let matches = App::new("pingas")
@@ -17,6 +20,13 @@ fn main() {
         .author(clap::crate_authors!())
         .set_term_width(80)
         .about("A pinger for jinglepings.")
+        .arg(
+            Arg::with_name("rate")
+                .short("r")
+                .help("The delay in milliseconds between pings for every pixel in the image.")
+                .takes_value(true)
+                .default_value(DEFAULT_PING_RATE),
+        )
         .arg(
             Arg::with_name("filename")
                 .help("A path to an image. Most bitmap format are supported.")
@@ -51,18 +61,83 @@ fn main() {
         )
         .get_matches();
 
-    let pinger = Pinger::new();
+    let rate = value_t_or_exit!(matches, "rate", u64);
+    let filename = matches.value_of("filename").unwrap();
+    let origin_x = value_t_or_exit!(matches, "x", u16);
+    let origin_y = value_t_or_exit!(matches, "y", u16);
+    let width = value_t_or_exit!(matches, "width", u32);
+    let height = value_t!(matches, "height", u32);
 
+    let image = {
+        let image = image::open(filename).unwrap_or_else(|err| {
+            eprintln!("Can't open file:\n{}", err);
+            exit(1);
+        });
+
+        // Either fit the image in the specified area if height is set or
+        // calculate the new height based on the given width
+        let height = height.unwrap_or_else(|_| {
+            ((width as f32) / (image.width() as f32) * (image.height() as f32)) as u32
+        });
+        image.resize(width, height, FilterType::Lanczos3).to_rgba()
+    };
+
+    // These are the dimensions of the resized image, they can be slightly
+    // different from the ones specified.
+    let image_height = image.height();
+    let image_width = image.width();
+
+    let pinger = Pinger::new();
     let ping_future = pinger
-        .map(|pinger| build_stream(&pinger, 5, 1074, 255, 255, 255, 255))
-        .and_then(|stream| stream.for_each(|_| Ok(println!("Ping!"))));
-    tokio::run(ping_future.map_err(|err| eprintln!("Error: {}", err)));
+        .map(move |pinger| {
+            let streams: Vec<_> = image
+                .enumerate_pixels()
+                .map(|(x, y, &Rgba([r, g, b, a]))| {
+                    build_stream(
+                        &pinger,
+                        rate,
+                        origin_x + x as u16,
+                        origin_y + y as u16,
+                        r,
+                        g,
+                        b,
+                        a,
+                    )
+                })
+                .collect();
+
+            streams
+        })
+        .map(|streams| {
+            for stream in streams {
+                tokio::spawn(
+                    stream
+                        .map_err(|err| {
+                            eprintln!("Error: {}", err);
+                            exit(1);
+                        })
+                        .for_each(|_| Ok(())),
+                );
+            }
+        });
+
+    println!(
+        "Printing '{}' to ({}, {}) @ {}x{} every {} ms",
+        filename, origin_x, origin_y, image_width, image_height, rate
+    );
+
+    tokio::run(ping_future.map_err(|err| {
+        eprintln!("Error: {}", err);
+        exit(1);
+    }));
 }
 
-/// Build a stream that pings a certain pixel every 100 milliseconds. Since the
+/// Build a stream that pings a certain pixel every n milliseconds. Since the
 /// server does not return a resposne all pings will time out.
+#[allow(clippy::many_single_char_names, clippy::too_many_arguments)]
 fn build_stream(
     pinger: &Pinger,
+    rate: u64,
     x: u16,
     y: u16,
     r: u8,
@@ -72,13 +147,14 @@ fn build_stream(
 ) -> impl Stream<Item = (), Error = tokio_ping::Error> {
     pinger
         .chain(build_address(x, y, r, g, b, a))
-        .timeout(Duration::from_millis(PING_RATE))
+        .timeout(Duration::from_millis(rate))
         .stream()
         .map(|_| ())
 }
 
 /// Build an IPv6 address for writing a pixel. `x` and `y` should correspond to
 /// some pixel on a 1920x1080 screen.
+#[allow(clippy::many_single_char_names)]
 fn build_address(x: u16, y: u16, r: u8, g: u8, b: u8, a: u8) -> IpAddr {
     IpAddr::V6(Ipv6Addr::new(
         0x2001,
